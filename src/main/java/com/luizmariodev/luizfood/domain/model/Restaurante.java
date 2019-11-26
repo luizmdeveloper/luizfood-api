c3d09290af9a9693828b2a528e8748f46bca58bb
luizmdeveloper <luizmariodev@gmail.com>
2019-11-25 21:39:11 -0300

    Adicionando suporte ao lombok

--- src/main/java/com/luizmariodev/luizfood/domain/model/Restaurante.java
+++ src/main/java/com/luizmariodev/luizfood/domain/model/Restaurante.java
@@ -9,10 +9,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "restaurantes")
 @Entity
 public class Restaurante {
 	
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -21,53 +27,4 @@ public class Restaurante {
 	
 	@Column(name="taxa_entrega")
 	private BigDecimal taxaEntrega;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public BigDecimal getTaxaEntrega() {
-		return taxaEntrega;
-	}
-
-	public void setTaxaEntrega(BigDecimal taxaEntrega) {
-		this.taxaEntrega = taxaEntrega;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Restaurante other = (Restaurante) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 }
