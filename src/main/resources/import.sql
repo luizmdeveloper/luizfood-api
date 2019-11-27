ee691e7b162ceff6bb432066507eec9ca3b35157
luizmdeveloper <luizmariodev@gmail.com>
2019-11-27 17:03:22 -0300

    Adicionando o controladores de cidade e estados. Implementados em ambos a buscar de totos registros

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+/* inserção na tabelas de cozinhas */
+INSERT INTO cozinhas (nome) VALUES ('Brasileira');
+INSERT INTO cozinhas (nome) VALUES ('Japônesa');
+INSERT INTO cozinhas (nome) VALUES ('Chinesa');
+INSERT INTO cozinhas (nome) VALUES ('Indiana');
+INSERT INTO cozinhas (nome) VALUES ('Tailandesa');
+INSERT INTO cozinhas (nome) VALUES ('Mexicana');
+
+/* inserção na tabelas de estados */
+INSERT INTO estados (nome) VALUES ('Pernambuco');
+INSERT INTO estados (nome) VALUES ('Bahia');
+INSERT INTO estados (nome) VALUES ('Paraíba');
+INSERT INTO estados (nome) VALUES ('Ceará');
+INSERT INTO estados (nome) VALUES ('Piauí');
+INSERT INTO estados (nome) VALUES ('Marnhão');
+INSERT INTO estados (nome) VALUES ('Rio grande do Norte');
