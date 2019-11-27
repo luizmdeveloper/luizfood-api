241fe0266e8a31f28ee776866372e0357906519c
luizmdeveloper <luizmariodev@gmail.com>
2019-11-27 19:53:47 -0300

    Adicionando a buscar por id em cidade e estado

--- src/main/java/com/luizmariodev/luizfood/controller/EstadoController.java
+++ src/main/java/com/luizmariodev/luizfood/controller/EstadoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,8 @@ public class EstadoController {
 		return estadoRepository.buscarTodos();
 	}
 	
-	
+	@GetMapping("/{id}")
+	public Estado buscarPorId(@PathVariable Long id) {
+		return estadoRepository.buscarPorId(id);
+	}
 }
