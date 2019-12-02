52f209129d4c4d7988737abd7e3fc8bb4429a91f
luizmdeveloper <luizmariodev@gmail.com>
2019-12-02 13:14:41 -0300

    Implementando inclusão, alteração, exclusão, buscar todos e buscar por id de cidade

---
+++ src/main/java/com/luizmariodev/luizfood/controller/CidadeController.java
@@ -0,0 +1,80 @@
+package com.luizmariodev.luizfood.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luizmariodev.luizfood.domain.exception.EntidadeEmUsoException;
+import com.luizmariodev.luizfood.domain.exception.EntidadeNaoEncontradaException;
+import com.luizmariodev.luizfood.domain.model.Cidade;
+import com.luizmariodev.luizfood.domain.repository.CidadeRepository;
+import com.luizmariodev.luizfood.domain.service.CidadeService;
+
+@RestController
+@RequestMapping
+public class CidadeController {
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private CidadeService cidadeService;
+	
+	@GetMapping
+	public List<Cidade> buscarTodos() {
+		return cidadeRepository.buscarTodos();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarPorCodigo(@PathVariable Long id) {
+		Cidade cidade = cidadeRepository.buscarPorId(id);
+		
+		if (cidade == null) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(cidade);
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> salvar(@RequestBody Cidade cidade) {
+		try {
+			cidadeService.salvar(cidade);
+			return ResponseEntity.status(HttpStatus.CREATED).build();
+		} catch (EntidadeEmUsoException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Cidade cidade) {
+		try {
+			Cidade cidadeSalva = cidadeService.atualizar(id, cidade);
+			return ResponseEntity.ok(cidadeSalva);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> excluir(@PathVariable Long id) {
+		try {
+			cidadeService.excluir(id);
+			return ResponseEntity.noContent().build();
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		} catch (EntidadeEmUsoException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+		}
+	}
+}
---
+++ src/main/java/com/luizmariodev/luizfood/domain/service/CidadeService.java
@@ -0,0 +1,67 @@
+package com.luizmariodev.luizfood.domain.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.luizmariodev.luizfood.domain.exception.EntidadeEmUsoException;
+import com.luizmariodev.luizfood.domain.exception.EntidadeNaoEncontradaException;
+import com.luizmariodev.luizfood.domain.model.Cidade;
+import com.luizmariodev.luizfood.domain.model.Estado;
+import com.luizmariodev.luizfood.domain.repository.CidadeRepository;
+import com.luizmariodev.luizfood.domain.repository.EstadoRepository;
+
+@Service
+public class CidadeService {
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	public Cidade salvar(Cidade cidade) {
+		Long estadoId = cidade.getEstado().getId();
+		Estado estado = buscarEstadoPorCodigo(estadoId);
+		cidade.setEstado(estado);
+		return cidadeRepository.salvar(cidade);
+	}
+	
+	public Cidade atualizar(Long id, Cidade cidade) {
+		Cidade cidadeSalvo = buscarCidadePorCodigo(id);
+		BeanUtils.copyProperties(cidade, cidadeSalvo, "id");
+		return cidadeRepository.salvar(cidadeSalvo);
+	}
+	
+	public void excluir(Long id) {
+		try {
+			cidadeRepository.exlcuir(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(String.format("Cidade com o código %d, não foi encontrada", id));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String.format("Cidade com o código %d, não pode ser excluída", id));
+		}
+	}
+	
+	private Cidade buscarCidadePorCodigo(Long cidadeId) {
+		Cidade cidade = cidadeRepository.buscarPorId(cidadeId);
+		
+		if (cidade == null) {
+			throw new EntidadeNaoEncontradaException(String.format("Cidade com o código %d, não foi encontrada", cidadeId));
+		}
+		
+		return cidade;
+	}
+
+	private Estado buscarEstadoPorCodigo(Long estadoId) {
+		Estado estado = estadoRepository.buscarPorId(estadoId);
+		
+		if (estado == null) {
+			throw new EntidadeNaoEncontradaException(String.format("Estado de código %d, não foi encontrada", estadoId));
+		}
+		
+		return estado;
+	}
+}
