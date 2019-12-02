d731472b0cdd7fdb990e670b278166d14c322fda
luizmdeveloper <luizmariodev@gmail.com>
2019-12-02 08:35:02 -0300

    Criando uma nova cozinha

--- src/main/java/com/luizmariodev/luizfood/controller/CozinhaController.java
+++ src/main/java/com/luizmariodev/luizfood/controller/CozinhaController.java
@@ -3,10 +3,14 @@ package com.luizmariodev.luizfood.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luizmariodev.luizfood.domain.model.Cozinha;
@@ -32,6 +36,11 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		else
 			return ResponseEntity.notFound().build();
-				
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void salvar(@RequestBody Cozinha cozinha) {
+		cozinhaRepository.salvar(cozinha);
 	}
 }
