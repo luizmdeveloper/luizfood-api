2bc159f79ee557c134ba0997295b2406f9733c9a
luizmdeveloper <luizmariodev@gmail.com>
2019-12-02 08:52:22 -0300

    Implementando a atualização de estado

--- src/main/java/com/luizmariodev/luizfood/controller/EstadoController.java
+++ src/main/java/com/luizmariodev/luizfood/controller/EstadoController.java
@@ -2,12 +2,14 @@ package com.luizmariodev.luizfood.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -43,5 +45,17 @@ public class EstadoController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Estado salvar(@RequestBody Estado estado) {
 		return estadoRepository.salvar(estado);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Estado> atualizar(@PathVariable Long id, @RequestBody Estado estado) {
+		Estado estadoAtual = estadoRepository.buscarPorId(id);
+		
+		if (estadoAtual != null) {
+			BeanUtils.copyProperties(estado, estadoAtual, "id");
+			return ResponseEntity.ok(estadoAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
