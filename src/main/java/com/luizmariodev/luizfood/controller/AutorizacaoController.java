cbb21ad17da1c83f19993c2bc6e94601ac2ed1ef
luizmdeveloper <luizmariodev@gmail.com>
2019-12-04 00:01:06 -0300

    Ajustes no retorno do código de status ao inserir

--- src/main/java/com/luizmariodev/luizfood/controller/AutorizacaoController.java
+++ src/main/java/com/luizmariodev/luizfood/controller/AutorizacaoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luizmariodev.luizfood.domain.exception.EntidadeEmUsoException;
@@ -48,6 +49,7 @@ public class AutorizacaoController {
 	} 
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Autorizacao salvar(@RequestBody Autorizacao autorizacao) {
 		return autorizacaoService.salvar(autorizacao);
 	}
