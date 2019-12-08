3b28dc55a8d5b5c5ad5b8caf0e16c7eee4c730a2
luizmdeveloper <luizmariodev@gmail.com>
2019-12-07 21:55:12 -0300

    Mapeando a classe de usuário

---
+++ src/main/java/com/luizmariodev/luizfood/domain/model/Usuario.java
@@ -0,0 +1,45 @@
+package com.luizmariodev.luizfood.domain.model;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+@Table(name="usuarios")
+public class Usuario {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private String email;
+	private String senha;
+	
+	@CreationTimestamp
+	@JoinColumn(name="data_cadastro")
+	private LocalTime dataCadastro;
+	
+	@ManyToMany
+	@JoinTable(name="usuarios_grupos",
+			   joinColumns = @JoinColumn(name="usuario_id"),
+			   inverseJoinColumns = @JoinColumn(name="permissao_id"))
+	private List<Grupo> grupos = new ArrayList<Grupo>();
+}
