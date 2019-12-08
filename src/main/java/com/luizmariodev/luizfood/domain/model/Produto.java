b42f8b827451c14ba65c4a7000ead370ef1a44f7
luizmdeveloper <luizmariodev@gmail.com>
2019-12-07 21:34:28 -0300

    Adicionando a class de produto

---
+++ src/main/java/com/luizmariodev/luizfood/domain/model/Produto.java
@@ -0,0 +1,35 @@
+package com.luizmariodev.luizfood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name="produtos")
+public class Produto {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private String descricao;
+	private BigDecimal valor;
+	private Boolean ativo;
+	
+	@ManyToOne
+	@JoinColumn(name="codigo_restaurante")
+	private Restaurante restaurante;
+}
