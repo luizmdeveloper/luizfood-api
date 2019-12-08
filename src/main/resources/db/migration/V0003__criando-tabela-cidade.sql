79f1120d10072192635ce8212965f6471135007e
luizmdeveloper <luizmariodev@gmail.com>
2019-12-08 18:08:05 -0300

    Adicionando a criação da talebela de cidades

---
+++ src/main/resources/db/migration/V0003__criando-tabela-cidade.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cidades (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    codigo_estado BIGINT NOT NULL,
+    
+    PRIMARY KEY(id),
+    CONSTRAINT fk_cidades FOREIGN KEY (codigo_estado) REFERENCES cidades (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
