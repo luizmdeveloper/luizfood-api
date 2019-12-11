00b4d882b6d102e80f31a7f32fc9fb0cb2ee8e84
luizmdeveloper <luizmariodev@gmail.com>
2019-12-10 22:15:04 -0300

    Refatorando buscar por id, alteração e exclusão de cidade

--- src/main/java/com/luizmariodev/luizfood/controller/CidadeController.java
+++ src/main/java/com/luizmariodev/luizfood/controller/CidadeController.java
@@ -12,10 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.luizmariodev.luizfood.domain.exception.EntidadeEmUsoException;
-import com.luizmariodev.luizfood.domain.exception.EntidadeNaoEncontradaException;
 import com.luizmariodev.luizfood.domain.model.Cidade;
 import com.luizmariodev.luizfood.domain.repository.CidadeRepository;
 import com.luizmariodev.luizfood.domain.service.CidadeService;
@@ -41,34 +40,20 @@ public class CidadeController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<?> salvar(@RequestBody Cidade cidade) {
-		try {
-			cidadeService.salvar(cidade);
-			return ResponseEntity.status(HttpStatus.CREATED).build();
-		} catch (EntidadeEmUsoException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cidade salvar(@RequestBody Cidade cidade) {
+		return cidadeService.salvar(cidade);
 	}
 	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Cidade cidade) {
-		try {
-			Cidade cidadeSalva = cidadeService.atualizar(id, cidade);
-			return ResponseEntity.ok(cidadeSalva);
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-		}
+		Cidade cidadeSalva = cidadeService.atualizar(id, cidade);
+		return ResponseEntity.ok(cidadeSalva);
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> excluir(@PathVariable Long id) {
-		try {
-			cidadeService.excluir(id);
-			return ResponseEntity.noContent().build();
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		} catch (EntidadeEmUsoException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-		}
+		cidadeService.excluir(id);
+		return ResponseEntity.noContent().build();
 	}
 }
