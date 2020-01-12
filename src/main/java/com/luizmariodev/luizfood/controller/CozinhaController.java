3dd10027f514fd7fabd6b276c6acd46a53ec3f3e
luizmdeveloper <luizmariodev@gmail.com>
2020-01-11 21:35:56 -0300

    Adicionando validações no contralador de cozinha

--- src/main/java/com/luizmariodev/luizfood/controller/CozinhaController.java
+++ src/main/java/com/luizmariodev/luizfood/controller/CozinhaController.java
@@ -2,6 +2,8 @@ package com.luizmariodev.luizfood.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -41,12 +43,12 @@ public class CozinhaController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cozinha salvar(@RequestBody Cozinha cozinha) {
+	public Cozinha salvar(@RequestBody @Valid Cozinha cozinha) {
 		return cozinhaService.salvar(cozinha);
 	}
 	
 	@PutMapping("/{id}")
-	public Cozinha atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha){
+	public Cozinha atualizar(@PathVariable Long id, @RequestBody @Valid Cozinha cozinha){
 		return cozinhaService.atualizar(id, cozinha);
 	}
 	
--- src/main/java/com/luizmariodev/luizfood/domain/model/Cozinha.java
+++ src/main/java/com/luizmariodev/luizfood/domain/model/Cozinha.java
@@ -5,6 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.luizmariodev.luizfood.GroupValidation;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,10 +19,13 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class Cozinha {
 	
+	@NotNull(groups = GroupValidation.SalvarRestaurante.class)
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;	
+	private Long id;
+	
+	@NotBlank
 	private String nome;
 
 }
