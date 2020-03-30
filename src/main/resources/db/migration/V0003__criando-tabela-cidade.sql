fde5805dafb9db203b9e3cbca9cbf62a66049dfe
luizmdeveloper <luizmariodev@gmail.com>
2020-03-30 10:02:49 -0300

    Ajustes na foreing key de cidade

--- src/main/resources/db/migration/V0003__criando-tabela-cidade.sql
+++ src/main/resources/db/migration/V0003__criando-tabela-cidade.sql
@@ -4,5 +4,5 @@ CREATE TABLE cidades (
     codigo_estado BIGINT NOT NULL,
     
     PRIMARY KEY(id),
-    CONSTRAINT fk_cidades FOREIGN KEY (codigo_estado) REFERENCES cidades (id)
+    CONSTRAINT fk_cidades FOREIGN KEY (codigo_estado) REFERENCES estados (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
