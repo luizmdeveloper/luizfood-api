3dcd0303f68b1f57cf52eefe629685aac9d3c77a
luizm <luizm@DESKTOP-MTDUC3K>
2020-05-16 17:57:40 -0300

    Refatorando o retorno de consultas paginadas

---
+++ src/main/java/com/luizmariodev/luizfood/core/jackson/PageJsonSerializer.java
@@ -0,0 +1,27 @@
+package com.luizmariodev.luizfood.core.jackson;
+
+import java.io.IOException;
+
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.Page;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+@JsonComponent
+public class PageJsonSerializer extends JsonSerializer<Page<?>> {
+
+	@Override
+	public void serialize(Page<?> value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+		gen.writeStartObject();
+		
+		gen.writeObjectField("content", value.getContent());
+		gen.writeObjectField("size", value.getSize());
+		gen.writeObjectField("totalElements", value.getTotalElements());
+		gen.writeObjectField("totalPages", value.getTotalPages());
+		gen.writeObjectField("number", value.getNumber());
+		
+		gen.writeEndObject();
+	}
+}
